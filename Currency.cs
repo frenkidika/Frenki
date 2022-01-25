49834bfe6900f37d778bbe7bdeb826a305fd8016
frenkidika <94971634+frenkidika@users.noreply.github.com>
2022-01-25 16:01:30 +0100

    Add files via upload

---
+++ Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Currency_Exchange2022.Models
+{
+    public class Currency
+    {  
+        public int ID_M { get; set; }
+
+        public int Value { get; set; }
+    }
+}
